b3cb3c0085026f9f78a727a64a34b30e74770d1d
Nikita Titov <nmtitov@yandex.ru>
2018-03-22 20:18:10 +0200

    Initial commit

---
+++ flatMapLatest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flatMapLatest
+//
+//  Created by Nikita Titov on 22/03/2018.
+//  Copyright © 2018 N. M. Titov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
